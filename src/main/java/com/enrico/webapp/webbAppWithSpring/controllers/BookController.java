4e90c4b8d840d6118a8187c9a8b819974c675c37
enricoGiga <enrico.gigante@gmail.com>
2017-09-12 14:52:02 +0200

    configuring Spring MVC controller

---
+++ src/main/java/com/enrico/webapp/webbAppWithSpring/controllers/BookController.java
@@ -0,0 +1,23 @@
+package com.enrico.webapp.webbAppWithSpring.controllers;
+
+import com.enrico.webapp.webbAppWithSpring.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository repository) {
+        this.bookRepository = repository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        model.addAttribute("books",bookRepository.findAll());
+
+        return "books";
+    }
+}
