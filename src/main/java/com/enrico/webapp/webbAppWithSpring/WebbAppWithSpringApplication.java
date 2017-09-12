6958a6e77ee1513f8e46543f4cb0260dbbc4cc66
enricoGiga <enrico.gigante@gmail.com>
2017-09-12 10:23:40 +0200

    adding JPA example

---
+++ src/main/java/com/enrico/webapp/webbAppWithSpring/WebbAppWithSpringApplication.java
@@ -0,0 +1,12 @@
+package com.enrico.webapp.webbAppWithSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbAppWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbAppWithSpringApplication.class, args);
+	}
+}
